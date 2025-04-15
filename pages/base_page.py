81a84edb9f27a0991c7a9d94c31e5a872de75708
Oleg Kiselev <oleg@i-kiselev.ru>
2025-04-15 20:37:56 +0300

    Add allure all pages

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
@@ -6,22 +7,27 @@ class BasePage:
     def __init__(self, driver):
         self.driver = driver
 
+    @allure.step("Подождать видимости элемента")
     def wait_for_element(self, locator, timeout=10):
         return WebDriverWait(self.driver, timeout).until(expected_conditions.visibility_of_element_located(locator))
 
+    @allure.step("Кликнуть на элемент")
     def click_on_element(self, locator, timeout=10):
         element = self.wait_for_element(locator, timeout)
         element.click()
 
+    @allure.step("Ввести текст в поле ввода")
     def send_keys_to_input(self, locator, keys, timeout=10):
         element = self.wait_for_element(locator, timeout)
         element.clear()
         element.send_keys(keys)
 
+    @allure.step("Получить текст элемента")
     def get_text_on_element(self, locator, timeout=10):
         element = self.wait_for_element(locator, timeout)
         return element.text
 
+    @allure.step("Скролл до элемента")
     def scroll_to_element(self, locator, timeout=10):
         element = self.wait_for_element(locator, timeout)
         self.driver.execute_script("arguments[0].scrollIntoView();", element)
--- pages/order_settings_page.py
+++ pages/order_settings_page.py
@@ -1,24 +1,31 @@
+import allure
 from pages.base_page import BasePage
 from locators.order_settings_locators import OrderSettingsLocators
 from data import *
 
 class OrderSettingsPage(BasePage):
 
+    @allure.step("Ввести дату")
     def date_deliver(self):
         self.send_keys_to_input(OrderSettingsLocators.WHEN, tomorrow)
 
+    @allure.step("Указать время аренды")
     def time_rent(self):
         self.click_on_element(OrderSettingsLocators.TIME_RENT)
         self.click_on_element(OrderSettingsLocators.ONE_DAY)
 
+    @allure.step("Кликнуть на кнопку Заказать")
     def click_but_order(self):
         self.click_on_element(OrderSettingsLocators.BUT_ORDER)
 
+    @allure.step("Кликнуть на кнопку Да в попапе")
     def click_yes_on_popup(self):
         self.click_on_element(OrderSettingsLocators.BUT_YES)
 
+    @allure.step("Кликнуть на активную дату в выпадающем списке")
     def click_active_date(self):
         self.click_on_element(OrderSettingsLocators.DATE_ACTIV)
 
+    @allure.step("Получить текст в попапе")
     def get_text_popup_order(self):
         return self.get_text_on_element(OrderSettingsLocators.TEXT_POPUP_ORDER)
